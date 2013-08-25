bebdfb0b081f825522527f8520f5167ad8222b88
Kaleb Clark <kaleb@abraxxus.net>
2013-08-25 07:32:59 -0700

    Added SQL queries and modified views to support multiple "things"

--- app/controllers/info_window_controller.rb
+++ app/controllers/info_window_controller.rb
@@ -1,7 +1,17 @@
 class InfoWindowController < ApplicationController
   def index
-    @thing = Thing.find_by_id(params[:thing_id])
-    if @thing.adopted?
+    #@thing = Thing.find_by_id(params[:thing_id])
+
+    @thing = Thing.find(
+      :all,
+      :joins => "LEFT JOIN types ON types.id = things.type_id",
+      :select => "things.*, types.title AS thing_title, types.action AS thing_action",
+      :conditions => ["things.id = ?", params[:thing_id]]
+      )
+    @thing = @thing[0]
+
+    #if @thing.adopted?
+    if !@thing.user_id.nil?
       if user_signed_in? && current_user.id == @thing.user_id
         render("users/thank_you")
       else
